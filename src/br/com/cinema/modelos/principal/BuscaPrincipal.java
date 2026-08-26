aae3e4e34411adff71fed1f3d5a98f782e068a31
celoramos <marceloramos20403@gmail.com>
2026-08-25 22:24:50 -0300

    busca de qualquer filme no BuscaPrincipal

--- src/br/com/cinema/modelos/principal/BuscaPrincipal.java
+++ src/br/com/cinema/modelos/principal/BuscaPrincipal.java
@@ -1,7 +1,10 @@
 package br.com.cinema.modelos.principal;
 import br.com.cinema.modelos.Titulo;
 import br.com.cinema.modelos.TituloOMDB;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
@@ -28,9 +31,9 @@ public class BuscaPrincipal {
         String json = response.body();
         System.out.println(json);
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         TituloOMDB meuTituloOMDB = gson.fromJson(json, TituloOMDB.class);
-        System.out.println(meuTituloOMDB);
+        System.out.println(meuTituloOMDB.title() + " - " + meuTituloOMDB.year() + " - " + meuTituloOMDB.runtime());
     }
 }
 
